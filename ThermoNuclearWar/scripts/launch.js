b211a20dd4daa8cd5f106e46c55ad7bbd3616c59
Stefano Chiodino <LouderTH@gmail.com>
2016-03-03 21:52:55 +0000

    Show a coloured message with the API status.

--- ThermoNuclearWar/scripts/launch.js
+++ ThermoNuclearWar/scripts/launch.js
@@ -1,21 +1,47 @@
 ﻿$(document).ready(function () {
-    var getSelectedCodeId = function () { return 1; };
-    $("#launch").click(function () {
+    checkStatusLoop();
+
+    var getSelectedCodeId = function() { return 1; };
+    $("#launch").click(function() {
         $.ajax({
             url: "/api/Launches?launchCodeId=" + getSelectedCodeId(),
             method: "POST"
         })
-        .done(function (data) {
+            .done(function(data) {
                 $("#resultPlaceholder").empty();
-                $("#resultPlaceholder").append( "<div class=\"alert alert-success alert-dismissible\" role=\"alert\">" +
+                $("#resultPlaceholder").append("<div class=\"alert alert-success alert-dismissible\" role=\"alert\">" +
                     "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>" +
-                    "Launch succesful!</div>" );
+                    "Launch succesful!</div>");
             })
-        .fail(function (data) {
-            $("#resultPlaceholder").empty();
-            $("#resultPlaceholder").append("<div class=\"alert alert-danger alert-dismissible\" role=\"alert\">" +
-                "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>" +
-                "Launch error!<br />" + JSON.parse(data.responseText).Message + "</div>");
-        });
+            .fail(function(data) {
+                $("#resultPlaceholder").empty();
+                $("#resultPlaceholder").append("<div class=\"alert alert-danger alert-dismissible\" role=\"alert\">" +
+                    "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>" +
+                    "Launch error!<br />" + JSON.parse(data.responseText).Message + "</div>");
+            });
     });
-})
+});
+
+function checkStatusLoop() {
+    $.ajax({
+        url: "/api/Launches"
+    })
+        .done(function(data) {
+            $("#status").removeClass("label-warning label-danger label-success");
+
+            if (data) {
+                $("#status").addClass("label-success");
+                $("#status").text("Status: online");
+            } else {
+                $("#status").addClass("label-danger");
+                $("#status").text("Status: offline");
+            }
+        })
+        .fail(function(data) {
+            $("#status").removeClass("label-warning label-danger label-success");
+            $("#status").addClass("label-warning");
+            $("#status").text("Couldn't retrieve status!");
+        });
+
+    window.setTimeout('checkStatusLoop()', 1000);
+}
